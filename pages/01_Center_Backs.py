ed4eef1b3ea302b9724b48515ac0ea90c35daeb9
Matthewduffy23 <matthewduffy2000@gmail.com>
2025-12-12 15:22:56 +0000

    Update 01_Center_Backs.py

--- pages/01_Center_Backs.py
+++ pages/01_Center_Backs.py
@@ -1125,7 +1125,7 @@ def make_ranking_image(
     footer_lines = _footer_lines_for_metric(metric_col, use_league_quality)[:3]
 
     # move divider up (more gap between row 10 and divider; no clash)
-    divider_y = 0.60
+    divider_y = 0.7
     ax.plot([footer_x_left, footer_x_right], [divider_y]*2, color="#E8E8E8", lw=0.9, zorder=2)
 
     # move footer up + increase line spacing (less congested)
